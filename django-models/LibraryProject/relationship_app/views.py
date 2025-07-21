8680b7274d2e1274a23a0cc9a13b17b43a189349
Kelvin Makau <kelvinmakau76@gmail.com>
2025-07-21 17:13:18 +0300

    function based

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.views.generic.detail import DetailView
 # Create your views here. Function based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
