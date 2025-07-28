15a61da40ce258536ba44e3046a97241466d7e01
Kelvin Makau <kelvinmakau76@gmail.com>
2025-07-28 12:27:26 +0300

    security

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,16 +1,31 @@
 from django.contrib import admin
-from .models import Book, Author, Library, Librarian, CustomUser, UserProfile
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser, Author
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Book admin
+# Custom admin for CustomUser
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_superuser")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    search_fields = ("username", "email")
+    ordering = ("username",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# Admin for Book
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author')  # Removed 'publication_year'
-    list_filter = ('author',)           # Removed 'publication_year'
-    search_fields = ('title', 'author__name')  # Searching through related author's name
+    list_display = ('title', 'author')
+    list_filter = ('author',)
+    search_fields = ('title',)
 
-# Register all relevant models
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
-admin.site.register(Library)
-admin.site.register(Librarian)
-admin.site.register(CustomUser)
-admin.site.register(UserProfile)
