7872601f476cba701e4c65dc981f1afd1a6719b2
Kelvin Makau <kelvinmakau76@gmail.com>
2025-07-28 08:15:31 +0300

    function based

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&l0mkl4$ntcx9q#2%fv&8l)ks+m=tea+v41^^416dv$0hp&m_y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
