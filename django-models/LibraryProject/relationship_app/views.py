ae69b87d121241888221263180b4c6335647cacb
Kelvin Makau <kelvinmakau76@gmail.com>
2025-07-28 09:15:37 +0300

    registration and login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.detail import DetailView
-from .models import Library, Book
+from .models import Library, Book, UserProfile
+from django.contrib.auth.decorators import user_passes_test
 
 #  Function-based view for listing all books
 def list_books(request):
@@ -31,3 +32,21 @@ def register_view(request):
     else:
         form = UserCreationForm()
         return render(request, 'relationship_app/register.html', {'form': form})
+
+# Check role
+def check_role(role):
+    def decorator(user):
+        return hasattr(user, 'userprofile') and user.userprofile.role == role
+    return decorator
+
+@user_passes_test(check_role('Admin'))
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(check_role('Librarian'))
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(check_role('Member'))
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
