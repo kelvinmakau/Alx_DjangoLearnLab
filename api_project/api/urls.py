6b2ecb434b6f1d6fd56d7094999716d3e4133923
Kelvin Makau <kelvinmakau76@gmail.com>
2025-08-06 13:52:57 +0300

    Adding a ListCreateAPIView to enable POST

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookList, BookViewSet
+from .views import BookList, BookViewSet, BookCreateList
 from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
@@ -8,6 +8,7 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book_list'),
+    path('books-create', BookCreateList.as_view(), name="book_create_list"),
     path('', include(router.urls)), # CRUD routes via router
     path('api-token-auth/', obtain_auth_token, name='api-auth-token'), # Token endpoint
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,9 +5,16 @@ from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here. 
+# BookList (ListAPIView allows only GET REQUESTS) - class BookList(generics.ListAPIView)
+# BookCreateList (ListCreateAPIView allows GET and POST requests)
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class =BookSerializer
+    permission_classes = [IsAuthenticated] # Requires authentication
+
+class BookCreateList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
     permission_classes = [IsAuthenticated] # Requires authentication
 
 class BookViewSet(viewsets.ModelViewSet): # Provides full CRUD operations (Create, Read, Update, Delete)
