0240bf65ec94586f0a4b223681b5803f7e70517f
Kelvin Makau <kelvinmakau76@gmail.com>
2025-08-06 09:23:07 +0300

    Token authentication is enabled by default for all classes

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+# Create your views here. 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class =BookSerializer
+    permission_classes = [IsAuthenticated] # Requires authentication
 
 class BookViewSet(viewsets.ModelViewSet): # Provides full CRUD operations (Create, Read, Update, Delete)
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated] # Requires authentication
