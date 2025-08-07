18c8b1f0001a22e578ea75864426226f154830b7
Kelvin Makau <kelvinmakau76@gmail.com>
2025-08-07 15:38:21 +0300

    Register my Book model, Return the real saved book object in the django admin page

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -5,3 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
     published_date = models.DateField()
+
+    # Returns the real saved book object in the django admin page, instead of Book object(1)
+    def __str__(self): 
+        return f"{self.title} by {self.author} published on {self.published_date}"
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xp&r=^&54ot)#fzgw*=!4hl3z4rn8*6l49a5=o7qh764pihz$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
