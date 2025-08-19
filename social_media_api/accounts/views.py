f653430f95f778a115c265a57f90af0f28946891
Kelvin Makau <kelvinmakau76@gmail.com>
2025-08-19 09:12:56 +0300

    Adding the views, login, profile and registeration

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from django.contrib.auth import login
+
+from .models import CustomUser
+from .serializers import UserSerializer, RegistrationSerializser, LoginSerializer
+
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegistrationSerializser
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'user': UserSerializer(user).data,
+            'token': token.key
+        }, status=status.HTTP_201_CREATED)
+    
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, created = Token.objects.get_or_create(user=user)
+        login(request, user)
+        return Response({
+            'user': UserSerializer(user).data,
+            'token': token.key
+        })
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
