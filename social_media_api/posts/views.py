b85e08a9f50a2a38de1190006125a81161316017
Kelvin Makau <kelvinmakau76@gmail.com>
2025-08-20 15:52:43 +0300

    Add Post viewset

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+
+class PostViewset(viewsets.ModelViewSet):
+    queryset = Post.objects.select_related('author').all() # Fetch related author data to reduce queries
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    # Filtering and searching
+    filterset_fields = ['author']
+    search_fields = ['title', 'content']
+    ordering_fields = ['created_at', 'updated_at', 'title']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
