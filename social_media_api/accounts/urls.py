71c39e7d51389f7c7f94e07e509e8284b8d18a21
Kelvin Makau <kelvinmakau76@gmail.com>
2025-08-21 15:38:03 +0300

    updating urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('follow/<int:user_id>', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<int:user_id>', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 
 ]
