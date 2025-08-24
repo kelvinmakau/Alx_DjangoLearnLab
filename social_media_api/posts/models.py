b8556c102dd5a4f0a03270c41013db548e975cd6
Kelvin Makau <kelvinmakau76@gmail.com>
2025-08-24 14:23:42 +0300

    Add the Like model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -29,3 +29,15 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post.title}'
+    
+# Likes Model
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user') # Makes sure a user cannot like the same post multiple times
+
+    def __str__(self):
+        return f'Like by {self.user} on {self.post.title}'
